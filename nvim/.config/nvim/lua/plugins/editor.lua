d16a4259c8c793b1a8b6cf01656ae8948fede572
shawVV1992 <798826105@163.com>
2023-06-09 16:32:16 +0800

    new changes by lazygit

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,1 +1,13 @@
-return { {} }
+return {
+  -- nvim - neo - tree
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      mappings = {
+        ["o"] = "open",
+        ["-"] = "open_split",
+        ["|"] = "open_vsplit",
+      },
+    },
+  },
+}
