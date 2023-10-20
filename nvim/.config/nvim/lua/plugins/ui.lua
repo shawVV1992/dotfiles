c64a651513cdc822d93c9d16b8c9ed8daf8a7f92
shaw <798826105@163.com>
2023-10-20 21:37:52 +0800

    change the stratup page

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -260,7 +260,7 @@ return {
     },
     opts = {
       options = {
-        always_show_bufferline = true,
+        -- always_show_bufferline = true,
         indicator = {
           -- icon = "▎", -- this should be omitted if indicator style is not 'icon'
           -- style = "icon" | "underline" | "none",
@@ -268,5 +268,71 @@ return {
         },
       },
     },
+  },
+
+  -- 启动页
+  {
+    "nvimdev/dashboard-nvim",
+    opts = function()
+      local logo = [[
+████████╗██╗  ██╗███████╗██╗   ██╗██╗███╗   ███╗
+╚══██╔══╝██║  ██║██╔════╝██║   ██║██║████╗ ████║
+   ██║   ███████║█████╗  ██║   ██║██║██╔████╔██║
+   ██║   ██╔══██║██╔══╝  ╚██╗ ██╔╝██║██║╚██╔╝██║
+   ██║   ██║  ██║███████╗ ╚████╔╝ ██║██║ ╚═╝ ██║
+   ╚═╝   ╚═╝  ╚═╝╚══════╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+  ]]
+
+      local slogen = "The computer is always right,and undebugged codes are always wrong."
+      logo = string.rep("\n", 8) .. logo .. "\n" .. slogen .. "\n\n"
+
+      local opts = {
+        theme = "doom",
+        hide = {
+          -- this is taken care of by lualine
+          -- enabling this messes up the actual laststatus setting after loading a file
+          statusline = false,
+        },
+        config = {
+          header = vim.split(logo, "\n"),
+      -- stylua: ignore
+      center = {
+        { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
+        { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
+        { action = "Telescope projects",                                        desc = " Projects",        icon = " ", key = "p" },
+        { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
+        { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
+        { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+        { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
+        { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
+        { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
+        { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+      },
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          end,
+        },
+      }
+
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+        button.key_format = "  %s"
+      end
+
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "DashboardLoaded",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+
+      return opts
+    end,
   },
 }
